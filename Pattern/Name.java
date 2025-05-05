707384126028af12a6b41793af23a3e2f9abb0ba
Nitin04kumar <04nitinkumar@gmail.com>
2025-05-05 08:57:11 +0530

    Upload

---
+++ Pattern/Name.java
@@ -0,0 +1,12 @@
+public class Name {
+    public static void main(String[] args) {
+        String name = "Dristi Priyadarshini";
+
+        for(int i = 0 ; i < name.length() ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                System.out.print(name.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+}
